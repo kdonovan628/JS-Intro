f9685d894e68bae11f941071bed32d691afbaa16
kdonovan628 <kelsey.donovan628@gmail.com>
2024-10-09 10:31:29 -0600

    Create index.html, index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Create a vault with the combination of 10 - 40 - 39
+// Find three different arithmetic operators that will generate each individual number
+// Have the combination display for the user in an alert popup
+
+const x = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+const numDogs = 5 * 2
+console.log (numDogs)
+
+const numCats = 56 - 16
+console.log (numCats)
+
+const numRabbits = 26 + 13 
+console.log (numRabbits)
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:10-40-39");
